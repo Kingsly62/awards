6b409eeb59f73bf850969b6a38f424194c1ce91c
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-03-11 20:42:42 +0300

    add new changes to login and register file templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
 
     path('', views.home, name='home'),
-    path('login/', views.login_view),
-    path('register/', views.register_view),
+    path('login/', views.login_view,name='login'),
+    path('register/', views.register_view,name='register'),
 
 ]
